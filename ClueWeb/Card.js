3d2ed7bea965ac6ef47c23cae638841d6249b59e
Benjamin Friedman <122704289+Benf1132@users.noreply.github.com>
2024-10-20 12:31:05 -0400

    Update Card.js

--- ClueWeb/Card.js
+++ ClueWeb/Card.js
@@ -1,4 +1,4 @@
-import { CardType } from './GameEnums.js';
+import { CardType, CharacterName, WeaponName, RoomName } from './GameEnums.js';
 
 class Card {
     constructor(type, name) {
@@ -14,6 +14,25 @@ class Card {
     // Getter for name
     getName() {
         return this.name;
+    }
+
+    // Method to get the image path
+    getImagePath() {
+        let imagePath = '';
+        switch (this.type) {
+            case CardType.SUSPECT:
+                imagePath = CharacterName[this.name]?.imagePath || null;
+                break;
+            case CardType.WEAPON:
+                imagePath = WeaponName[this.name]?.imagePath || null;
+                break;
+            case CardType.ROOM:
+                imagePath = RoomName[this.name]?.imagePath || null;
+                break;
+            default:
+                imagePath = 'images/default.png';
+        }
+        return imagePath;
     }
 
     // toString method for easy printing
