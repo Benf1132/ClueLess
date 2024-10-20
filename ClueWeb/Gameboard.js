af43693a84f691c5eff22d67c7feb72984809365
Benjamin Friedman <122704289+Benf1132@users.noreply.github.com>
2024-10-20 14:39:07 -0400

    Update Gameboard.js

--- ClueWeb/Gameboard.js
+++ ClueWeb/Gameboard.js
@@ -57,7 +57,6 @@ class Gameboard {
 
             this.tiles[row][col] = tileObj;
             tileObj.element = tile;  // Attach the DOM element to the tile object
-            console.log(`Tile at row ${row}, col ${col} initialized as ${tileObj.constructor.name}`);
         });
     }
 
