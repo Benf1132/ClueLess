20c463803a1433f415d939dca86892aa2b2c0c58
Benjamin Friedman <122704289+Benf1132@users.noreply.github.com>
2024-10-28 15:25:14 -0400

    Update Player.js

--- ClueWeb/Player.js
+++ ClueWeb/Player.js
@@ -1,5 +1,10 @@
 import Hand from './Hand.js';
 import Hallway from './Hallway.js';
+import { CharacterName, WeaponName, RoomName } from './GameEnums.js';
+
+function getEnumName(enumType, key) {
+    return enumType[key]?.name || key;
+}
 
 class Player {
     constructor(username = '', password = '', character = null) {
@@ -11,22 +16,18 @@ class Player {
         this.pulledBySuggestion = false;
     }
 
-    // Getter for the username
     getUsername() {
         return this.username;
     }
 
-    // Getter for the password
     getPassword() {
         return this.password;
     }
 
-    // Check if the player is active
     isActive() {
         return this.activePlayer;
     }
 
-    // Set the player as inactive and update hallway occupancy
     setInactivity() {
         this.activePlayer = false;
 
@@ -36,50 +37,46 @@ class Player {
         }
     }
 
-    // Check if the player was pulled by a suggestion
     wasPulledBySuggestion() {
         return this.pulledBySuggestion;
     }
 
-    // Set if the player was pulled by a suggestion
     setPulledBySuggestion(pulledBySuggestion) {
         this.pulledBySuggestion = pulledBySuggestion;
     }
 
-    // Getter for the character
     getCharacter() {
         return this.character;
     }
 
-    // Getter for the player's hand
     getHand() {
         return this.hand;
     }
-    // Enhanced getMatchingCards method with debug logs
-    getMatchingCards(suspect, weapon, room) {
-        return this.hand.getCards().filter(card => {
-            // Retrieve the actual names for comparison
-            const suspectName = CharacterName[suspect]?.name;
-            const weaponName = WeaponName[weapon]?.name;
-            const roomName = RoomName[room]?.name;
 
+    // Enhanced getMatchingCards method with standardized enum names
+    getMatchingCards(suspect, weapon, room) {
+        const suspectName = getEnumName(CharacterName, suspect);
+        const weaponName = getEnumName(WeaponName, weapon);
+        const roomName = getEnumName(RoomName, room);
+
+        return this.hand.getCards().filter(card => {
             const matchesSuspect = card.getName() === suspectName;
             const matchesWeapon = card.getName() === weaponName;
             const matchesRoom = card.getName() === roomName;
+
+            console.log(`Checking card ${card.getName()} against suspect: ${matchesSuspect}, weapon: ${matchesWeapon}, room: ${matchesRoom}`);
+            return matchesSuspect || matchesWeapon || matchesRoom;
         });
     }
 
-    // Set the player's username
     setUsername(username) {
         this.username = username;
     }
 
-    // Set the player's password
     setPassword(password) {
         this.password = password;
     }
 
-    // Set the player's character
     setCharacter(character) {
         this.character = character;
     }
